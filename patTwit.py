b7174ec8d98ba7f8aee6ac61a1f08637f9ada5ee
Patrick <patatoa@gmail.com>
2017-10-12 21:20:40 -0500

    Following along with book. pulled down trends and found intersection of world and us trends

--- patTwit.py
+++ patTwit.py
@@ -1,5 +1,6 @@
-import twitter
 import os
+import json
+import twitter
 
 CONSUMER_KEY = 'qd35nCDinA4DLRpspdvPtMdmH'
 OAUTH_TOKEN = '101673271-XTZn6oQlFkBmpCj7mX5zGNHtrfoNctBO4sz0cuVv'
@@ -9,8 +10,20 @@ try:
 except KeyError as e:
     print(type(e))
 
+WORLD_WOE_ID = 1
+US_WOE_ID = 23424977
+
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 twitter_api = twitter.Twitter(auth=auth)
 
-print(twitter_api)
+world_trends = twitter_api.trends.place(_id = WORLD_WOE_ID)
+
+us_trends = twitter_api.trends.place(_id = US_WOE_ID)
+
+world_trends_set = set([trend['name'] for trend in world_trends[0]['trends']])
+us_trends_set = set([trend['name'] for trend in us_trends[0]['trends']])
+
+common_trends = world_trends_set.intersection(us_trends_set)
+
+print(common_trends)
